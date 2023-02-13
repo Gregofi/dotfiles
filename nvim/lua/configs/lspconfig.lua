8dbf385020527174dfbf18ee87149a87f79f5005
Filip Gregor <filip.gregor98@gmail.com>
2023-02-13 18:28:10 +0100

    Add bar

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,1 +1,20 @@
 require'lspconfig'.clangd.setup{}
+require'lspconfig'.rust_analyzer.setup{}
+
+-- Show window with the error on cursor
+vim.o.updatetime = 250
+vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+vim.api.nvim_create_autocmd("CursorHold", {
+  buffer = bufnr,
+  callback = function()
+    local opts = {
+      focusable = false,
+      close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
+      border = 'rounded',
+      source = 'always',
+      prefix = ' ',
+      scope = 'cursor',
+    }
+    vim.diagnostic.open_float(nil, opts)
+  end
+})
