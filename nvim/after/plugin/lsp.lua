fc5d67fd69f38ffff0ff6f01d5af82b6aa6a04fb
Filip Gregor <filip.gregor98@gmail.com>
2023-07-06 19:57:55 +0200

    Make errors show only in float window on keypress.
    The hovers (K) were interfereing with the errors. Now both must be
    explicitly requested by keypress.

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -48,22 +48,7 @@ lsp.on_attach(function(client, bufnr)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 
     -- Show window with the error on cursor
-    vim.o.updatetime = 250
-    vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
-    vim.api.nvim_create_autocmd("CursorHold", {
-      buffer = bufnr,
-      callback = function()
-        local opts = {
-          focusable = false,
-          close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-          border = 'rounded',
-          source = 'always',
-          prefix = ' ',
-          scope = 'cursor',
-        }
-        vim.diagnostic.open_float(nil, opts)
-      end
-    })
+    vim.keymap.set("n", "<leader>vse", function() vim.diagnostic.open_float(nil, {focus=false}) end)
 end)
 
 lsp.setup()
