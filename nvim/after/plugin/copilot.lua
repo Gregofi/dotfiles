d3444e3d6aec4cd6551e1728766e4865c5aaad10
Filip Gregor <filip.gregor98@gmail.com>
2023-08-26 12:33:48 +0200

    add copilot

---
+++ nvim/after/plugin/copilot.lua
@@ -0,0 +1,4 @@
+vim.g.copilot_no_tab_map = true
+-- We have to do this as raw vim script because using lua remap
+-- causes an escape characters to be inserted at the end.
+vim.cmd('imap <silent><script><expr> <C-J> copilot#Accept("\\<CR>")')
