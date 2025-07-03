69577c6e01ee515ac3b039d7ed295028a265f710
Filip Gregor <filip.gregor@firma.seznam.cz>
2025-07-03 13:30:58 +0200

    nvim: search in hidden files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,6 +8,17 @@ Plugin.dependencies = {
 
 Plugin.cmd = {'Telescope'}
 
+Plugin.opts = {
+    pickers = {
+        live_grep = {
+            additional_args = function(opts)
+                -- search in CI files (.github, .gitlab-ci.yml, etc.)
+                return {'--hidden', '--glob', '!.git/*'}
+            end
+        },
+    },
+}
+
 function Plugin.init()
     local ts = require('telescope.builtin')
     vim.keymap.set('n', '<leader>pf', ts.find_files, {})
